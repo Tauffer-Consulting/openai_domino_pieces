593417f0468d3f0902cc2f521fd4e2e75a62c96c
giupb <giulio.pbrugalli@gmail.com>
2023-04-26 16:09:51 -0300

    improve text summarizer input model

--- pieces/TextSummarizerPiece/models.py
+++ pieces/TextSummarizerPiece/models.py
@@ -3,14 +3,35 @@ from pydantic import BaseModel, Field
 class InputModel(BaseModel):
     """Text Summarizer Piece"""
 
+    prompt_template: str = Field(
+        default="""Write a summary of the given text while maintaining its original writing form.
+
+text:
+    
+{text}
+
+CONCISE SUMMARY:""",
+        description="Prompt template to use for summarization"
+    )
+    
     text: str = Field(
         default="",
-        description="text to summarize",
+        description="Text to summarize",
     )
 
     openai_model_name: str = Field(
         default="gpt-3.5-turbo",
-        description="model name to use for summarization"
+        description="OpenAI model name to use for summarization"
+    )
+
+    max_tokens: int = Field(
+        default=None,
+        description="The maximum number of tokens to generate in the summary."
+    )
+
+    temperature: float = Field(
+        description="Temperature of the model, between 0 (more precise) and 1 (more creative)",
+        default=0.2
     )
 
 
--- pieces/TextSummarizerPiece/piece.py
+++ pieces/TextSummarizerPiece/piece.py
@@ -4,22 +4,17 @@ import openai
 
 class TextSummarizerPiece(BasePiece):
     def piece_function(self, input_model: InputModel):
-        prompt_template=f"""Write a summary of the given text while maintaining its original writing form, whether it is in the first, second, or third person.
-
-text:
-    
-"{input_model.text}"
-
-CONCISE SUMMARY:"""
-
+        prompt = input_model.prompt_template.format(text=input_model.text)
         openai.api_key = self.secrets.OPENAI_API_KEY
+        
         response = openai.ChatCompletion.create(
             model = input_model.openai_model_name,
             messages = [
-                {"role": "user", "content": prompt_template}
+                {"role": "user", "content": prompt}
             ],
-            temperature = 0.3
-        )
+            temperature = input_model.temperature,
+            max_tokens = input_model.max_tokens,
+        )            
 
         return OutputModel(
             summarized_text = response['choices'][0]['message']['content']
