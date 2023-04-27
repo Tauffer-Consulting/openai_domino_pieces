1f355941363097958a9a22757f58890fdc97bf9d
giupb <giulio.pbrugalli@gmail.com>
2023-04-27 16:09:20 -0300

    multiple chunks rounds

--- pieces/TextSummarizerPiece/models.py
+++ pieces/TextSummarizerPiece/models.py
@@ -1,8 +1,17 @@
 from pydantic import BaseModel, Field
 from enum import Enum
-class LLMModelType(str, Enum):
-    gpt_3_5_turbo = "gpt-3.5-turbo"
-    gpt_4 = "gpt-4"
+class LLMModelType(tuple, Enum):
+    GPT_3_5_TURBO = ("gpt-3.5-turbo", 4000)
+    GPT_4 = ("gpt-4", 8000)
+
+    @property
+    def model_name(self):
+        return self.value[0]
+
+    @property
+    def token_limits(self):
+        return self.value[1]
+
 class InputModel(BaseModel):
     """Text Summarizer Piece"""    
     text: str = Field(
@@ -12,21 +21,21 @@ class InputModel(BaseModel):
 
     chunk_size: int = Field(
         default=1000,
-        description="Chunk size of each pre-summary chunk"
+        description="Chunk size, measured in tokens, of each pre-summary chunk"
     )
 
-    chunk_overlap: int = Field(
-        default=20,
-        description="Chunk overlap of each pre-summary chunk"
+    chunk_overlap_rate: float = Field(
+        default=0.2,
+        description="The percentage of overlap between each chunk"
     )
 
-    openai_model_name: LLMModelType = Field(
-        default=LLMModelType.gpt_3_5_turbo,
+    openai_model: LLMModelType = Field(
+        default=LLMModelType.GPT_3_5_TURBO,
         description="OpenAI model name to use for summarization"
     )
 
     max_tokens: int = Field(
-        default=None,
+        default=500,
         description="The maximum number of tokens to generate in the summary."
     )
 
--- pieces/TextSummarizerPiece/piece.py
+++ pieces/TextSummarizerPiece/piece.py
@@ -18,7 +18,7 @@ CONCISE SUMMARY:"""
     
     async def chat_completion_method(self, input_model: InputModel, text: str):
         response = openai.ChatCompletion.create(
-            model = input_model.openai_model_name,
+            model = input_model.openai_model.model_name,
             messages = [
                 {"role": "user", "content": text}
             ],
@@ -32,23 +32,36 @@ CONCISE SUMMARY:"""
         tasks = [self.chat_completion_method(input_model, text) for text in texts_chunks]
         return await asyncio.gather(*tasks)
     
-    def piece_function(self, input_model: InputModel):        
-        encoding = tiktoken.encoding_for_model(input_model.openai_model_name)
-        len_chunk_text = input_model.chunk_size - len(encoding.encode(text=self.prompt))
-        full_text_tokens = encoding.encode(text=input_model.text)
+    def create_chunks_with_prompt(self, input_model: InputModel, text: str):
+        encoding = tiktoken.encoding_for_model(input_model.openai_model.model_name)
+        text_chunk_size = input_model.chunk_size - len(encoding.encode(text=self.prompt))
+        full_text_tokens = encoding.encode(text=text)
+        chunk_overlap = round(input_model.chunk_overlap_rate * text_chunk_size)
         texts_chunks_with_prompt = []
-        for i in range(0, len(full_text_tokens), len_chunk_text):
-            idx_chunk_start = [i - input_model.chunk_overlap if i>0 else 0][0]
-            decoded_text_chunk = encoding.decode(full_text_tokens[idx_chunk_start:i+len_chunk_text])
+        for i in range(0, len(full_text_tokens), text_chunk_size):
+            idx_chunk_start = [i - chunk_overlap if i>0 else 0][0]
+            decoded_text_chunk = encoding.decode(full_text_tokens[idx_chunk_start:i+text_chunk_size])
             text_chunk_with_prompt = self.prompt.format(text=decoded_text_chunk) 
             texts_chunks_with_prompt.append(text_chunk_with_prompt)
+        return text_chunk_with_prompt
         
+    
+    def piece_function(self, input_model: InputModel):                
         openai.api_key = self.secrets.OPENAI_API_KEY
-        
         loop = asyncio.new_event_loop()
-        summaries_chunks = loop.run_until_complete(self.agenerate_chat_completion(input_model, texts_chunks_with_prompt))
-        joined_summaries_chunks = [" ".join(summaries_chunks)]
-        final_summary = loop.run_until_complete(self.agenerate_chat_completion(input_model, joined_summaries_chunks))
+
+        encoding = tiktoken.encoding_for_model(input_model.openai_model.model_name)
+        token_limits = input_model.openai_model.token_limits
+        completion_max_token = input_model.max_tokens
+        text_token_count = token_limits
+        text = input_model.text
+        while text_token_count >= (token_limits - completion_max_token):
+            texts_chunks_with_prompt = self.create_chunks_with_prompt(input_model=input_model, text=text)
+            summaries_chunks = loop.run_until_complete(self.agenerate_chat_completion(input_model, texts_chunks_with_prompt))
+            text = " ".join(summaries_chunks)
+            text_token_count = len(encoding.encode(text=text))
+
+        final_summary = loop.run_until_complete(self.agenerate_chat_completion(input_model, [text]))
 
         return OutputModel(
             summarized_text = final_summary
