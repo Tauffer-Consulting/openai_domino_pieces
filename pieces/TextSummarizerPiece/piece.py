752f41ce36c378fda83c0c65847afa1bdebb8b1e
giupb <giulio.pbrugalli@gmail.com>
2023-04-28 09:56:03 -0300

    small fixes

--- pieces/TextSummarizerPiece/piece.py
+++ pieces/TextSummarizerPiece/piece.py
@@ -37,13 +37,13 @@ CONCISE SUMMARY:"""
         text_chunk_size = input_model.chunk_size - len(encoding.encode(text=self.prompt))
         full_text_tokens = encoding.encode(text=text)
         chunk_overlap = round(input_model.chunk_overlap_rate * text_chunk_size)
-        texts_chunks_with_prompt = []
+        text_chunks_with_prompt = []
         for i in range(0, len(full_text_tokens), text_chunk_size):
             idx_chunk_start = [i - chunk_overlap if i>0 else 0][0]
             decoded_text_chunk = encoding.decode(full_text_tokens[idx_chunk_start:i+text_chunk_size])
-            text_chunk_with_prompt = self.prompt.format(text=decoded_text_chunk) 
-            texts_chunks_with_prompt.append(text_chunk_with_prompt)
-        return text_chunk_with_prompt
+            chunk_with_prompt = self.prompt.format(text=decoded_text_chunk) 
+            text_chunks_with_prompt.append(chunk_with_prompt)
+        return text_chunks_with_prompt
         
     
     def piece_function(self, input_model: InputModel):                
@@ -55,7 +55,7 @@ CONCISE SUMMARY:"""
         completion_max_token = input_model.max_tokens
         text_token_count = token_limits
         text = input_model.text
-        while text_token_count >= (token_limits - completion_max_token):
+        while text_token_count > (token_limits - completion_max_token):
             texts_chunks_with_prompt = self.create_chunks_with_prompt(input_model=input_model, text=text)
             summaries_chunks = loop.run_until_complete(self.agenerate_chat_completion(input_model, texts_chunks_with_prompt))
             text = " ".join(summaries_chunks)
@@ -64,6 +64,6 @@ CONCISE SUMMARY:"""
         final_summary = loop.run_until_complete(self.agenerate_chat_completion(input_model, [text]))
 
         return OutputModel(
-            summarized_text = final_summary
+            summarized_text = final_summary[0]
         )
 
