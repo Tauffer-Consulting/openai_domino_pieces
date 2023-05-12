dda7846ce6f58cef1e398b214e7211e9cf649e8b
giupb <giulio.pbrugalli@gmail.com>
2023-05-12 16:00:57 -0300

    PromptForImageGeneratorPiece reviewed

--- pieces/PromptForImageGeneratorPiece/models.py
+++ pieces/PromptForImageGeneratorPiece/models.py
@@ -2,44 +2,59 @@ from pydantic import BaseModel, Field
 from enum import Enum
 
 class LLMModelType(str, Enum):
+    """
+    OpenAI model types
+    """
+
     GPT_3_5_TURBO = "gpt-3.5-turbo"
     GPT_4 = "gpt-4"
 
 class InputModel(BaseModel):
-    """MyNewPiece Input"""
+    """
+    Prompt For Image Generator Piece Input
+    """
+
     context: str = Field(
-        default=None,
+        ...,
         description="The context to generate an image from",
     )
-
+    output_file_name: str = Field(
+        default=None,
+        description="Use it only if you want to save the prompt result to a file in addition to the string output"
+    )
     openai_model: LLMModelType = Field(
         default=LLMModelType.GPT_3_5_TURBO,
         description="OpenAI model to bring your character to life"
     )
-
     completion_max_tokens: int = Field(
-        default=500,
-        description="The maximum number of tokens to generate the new history."
+        default=350,
+        description="The maximum number of tokens to generate the prompt."
     )
-
     temperature: float = Field(
         default=0.7,
-        description="Temperature of the model, between 0 (more precise) and 1 (more creative)"
+        description="Temperature of the model, between 0 (more precise) and 1 (more creative)",
+        gt=0,
+        le=1
     )
 
 class OutputModel(BaseModel):
-    """MyNewPiece Output"""
-    generated_prompt: str = Field(
+    """
+    Prompt For Image Generator Piece Output
+    """
+
+    generated_prompt_string: str = Field(
         description="The generated prompt to pass to an image generator AI",
     )
-
-    prompt_file_path: str = Field(
+    generated_prompt_file_path: str = Field(
+        default=None,
         description="The path to the generated prompt, in .txt format",
     )
 
 class SecretsModel(BaseModel):
-    """MyNewPiece Secrets"""
-    
+    """
+    Prompt For Image Generator Piece Secrets
+    """    
+
     OPENAI_API_KEY: str = Field(
         description="Your OpenAI API key"
     )
--- pieces/PromptForImageGeneratorPiece/piece.py
+++ pieces/PromptForImageGeneratorPiece/piece.py
@@ -3,16 +3,6 @@ from .models import InputModel, OutputModel
 import openai
 
 class PromptForImageGeneratorPiece(BasePiece):
-    template = """You have access to an AI that generates images through text prompts. 
-Your function is to write a prompt for this AI from a given context. 
-You  always write a short prompt that is designed to help the image generator AI create an image for the given context. 
-You are very good at writing these text prompts for any context that is given  to you. 
-You're very creative in how you describe the context you've been given, and like to vary the mood that runs through the prompt you've written. 
-It also always suggests some specific art style for the AI to create the image. You know many art styles, so you always vary a lot on your suggestions!
-Now, create a prompt to help the image generator AI to create an image for this context:
-{context}
-"""
-
     def openai_chat_completion(self, input_model: InputModel, prompt: str, temperature: float = None):
         response = openai.ChatCompletion.create(
             model = input_model.openai_model,
@@ -26,15 +16,31 @@ Now, create a prompt to help the image generator AI to create an image for this 
         return response['choices'][0]['message']['content']
 
     def piece_function(self, input_model: InputModel):
+        template = """You have access to an AI that generates images through text prompts. 
+Your function is to write a prompt for this AI from a given context. 
+You  always write a short prompt that is designed to help the image generator AI create an image for the given context. 
+You are very good at writing these text prompts for any context that is given  to you. 
+You're very creative in how you describe the context you've been given, and like to vary the mood that runs through the prompt you've written. 
+It also always suggests some specific art style for the AI to create the image. You know many art styles, so you always vary a lot on your suggestions!
+Now, create a prompt to help the image generator AI to create an image for this context:
+{context}"""
+
         openai.api_key = self.secrets.OPENAI_API_KEY
-        prompt = self.template.format(context=input_model.context)
+        prompt = template.format(context=input_model.context)
         generated_prompt = self.openai_chat_completion(input_model, prompt)
-        output_file_path = output_file_path = f"{self.results_path}/generated_prompt.txt"
-   
+
+        if not input_model.output_file_name:
+            return OutputModel(
+                generated_prompt_string=generated_prompt,
+            )
+        
+        output_file_path = f"{self.results_path}/{input_model.output_file_name}.txt"
         with open(output_file_path, "w") as f:
             f.write(generated_prompt)
         
+        self.logger.info(f"Prompt file saved at: {output_file_path}")
+    
         return OutputModel(
-            generated_prompt=generated_prompt,
-            prompt_file_path=output_file_path
+            generated_prompt_string=generated_prompt,
+            generated_prompt_file_path=output_file_path
         )
