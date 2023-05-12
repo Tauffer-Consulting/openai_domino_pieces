88e0e09deb54caf65f3a0924dcc4094acaf6eced
giupb <giulio.pbrugalli@gmail.com>
2023-05-12 10:40:29 -0300

    improve the input and output models

--- pieces/CompletionsPiece/models.py
+++ pieces/CompletionsPiece/models.py
@@ -1,8 +1,22 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, FilePath
 from enum import Enum
+from typing import Union
+
+class OutputTypeType(str, Enum):
+    """
+    Output type for the completion result
+    """
+
+    string = "string"
+    file = "file"
 
 
 class LLMModelType(str, Enum):
+    """
+    The name of the model to use.
+    Options: ada, babbage, curie, davinci
+    """
+
     ada = "text-ada-001"
     babbage = "text-babbage-001"
     curie = "text-curie-001"
@@ -10,38 +24,60 @@ class LLMModelType(str, Enum):
 
 
 class InputModel(BaseModel):
-    llm_model_name: LLMModelType = Field(
-        description="The name of the model to use. Options: ada, babbage, curie, davinci"
-    )
+    """
+    Completions Piece input model
+    """
+
     prompt: str = Field(
+        default=None,
         description="The prompt to use for the completion"
     )
+    prompt_file_path: str = Field(
+        default=None,
+        description="Use only if not using prompt field. The path to a file containing the prompt to use for the completion. Must be .txt format",
+    )
+    output_type: OutputTypeType = Field(
+        default=OutputTypeType.string,
+        description="The type of output to return"
+    )
+    llm_model_name: LLMModelType = Field(
+        default=LLMModelType.babbage,
+        description="The name of the model to use. Options: ada, babbage, curie, davinci"
+    )
     max_tokens: int = Field(
-        description="The maximum number of tokens to generate in the completion.",
-        default=16,
+        default=None,
+        description="The maximum number of tokens to generate in the completion",
         gt=0
     )
     temperature: float = Field(
-        description="What sampling temperature to use, between 0 and 2.",
         default=1.,
+        description="What sampling temperature to use, between 0 and 2",
         gt=0.,
         le=2.
     )
 
 
 class OutputModel(BaseModel):
+    """
+    Completions Piece output model
+    """
+    
     message: str = Field(
         description="Output message to log"
     )
-    result_text: str = Field(
-        description="The result of the text completion."
+    completion_result: Union[str, FilePath] = Field(
+        description="The result of the completion"
     )
     usage_total_tokens: int = Field(
-        description="The total number of tokens used in the completion."
+        description="The total number of tokens used in the completion"
     )
 
 
 class SecretsModel(BaseModel):
+    """
+    Completions Piece secret model
+    """
+
     OPENAI_API_KEY: str = Field(
         description="OpenAI API key"
     )
--- pieces/CompletionsPiece/piece.py
+++ pieces/CompletionsPiece/piece.py
@@ -10,20 +10,22 @@ class CompletionsPiece(BasePiece):
     """
     
     def piece_function(self, input_model: InputModel):
-
-        # Secrets are retrieved from ENV vars
-        openai_api_key = os.environ.get("OPENAI_API_KEY", None)
+        openai_api_key = self.secrets.OPENAI_API_KEY
         if openai_api_key is None:
             raise Exception("OPENAI_API_KEY not found in ENV vars. Please add it to the secrets section of the Piece.")
         openai.api_key = openai_api_key
 
         # Input arguments are retrieved from the Input model object
         llm_model_name = input_model.llm_model_name
-        prompt = input_model.prompt
         max_tokens = input_model.max_tokens
         temperature = input_model.temperature
 
-        print("Making OpenAI completion request...")
+        if input_model.prompt_file_path:
+            with open(input_model.prompt_file_path, "r") as f:
+                prompt = f.read()
+        else: prompt = input_model.prompt
+
+        self.logger.info("Making OpenAI completion request...")
         try:
             r = openai.Completion.create(
                 model=llm_model_name,
@@ -32,16 +34,27 @@ class CompletionsPiece(BasePiece):
                 temperature=temperature
             )
             r_dict = r.to_dict_recursive()
-            result_text = r_dict["choices"][0]["text"]
+            completion_result = r_dict["choices"][0]["text"]
             usage_total_tokens = r_dict["usage"]["total_tokens"]
             message = "\nCompletion task was successful!"
         except Exception as e:
-            print(f"\nCompletion task failed: {e}")
+            self.logger.info(f"\nCompletion task failed: {e}")
             raise Exception(f"Completion task failed: {e}")
+        
+        if input_model.output_type == "string":
+            return OutputModel(
+                message=message,
+                completion_result=completion_result,
+                usage_total_tokens=usage_total_tokens
+            )
+
+        output_file_path = f"{self.results_path}/completion_result.txt"
+        with open(output_file_path, "w") as f:
+            f.write(completion_result)
 
         # Finally, results should return as an Output model
         return OutputModel(
             message=message,
-            result_text=result_text,
+            completion_result=output_file_path,
             usage_total_tokens=usage_total_tokens
         )
