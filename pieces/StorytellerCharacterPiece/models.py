3f915cc56681e6690ce3fa17f932958f85bc45b9
giupb <giulio.pbrugalli@gmail.com>
2023-05-12 17:59:44 -0300

    small fixes

--- pieces/StorytellerCharacterPiece/models.py
+++ pieces/StorytellerCharacterPiece/models.py
@@ -6,32 +6,30 @@ class LLMModelType(str, Enum):
     GPT_4 = "gpt-4"
 
 class InputModel(BaseModel):
-    """MyNewPiece Input"""
+    """
+    Storyteller Character Input
+    """
+
     character_name: str = Field(
         ...,
         description="Your character's name",
     )
-
     character_description: str = Field(
         ...,
         description="Your character's description",
     )
-
     previous_stories_file_path: str = Field(
         default=None,
         description="Path to your character's previous stories. Must be a .txt file",
     )
-
     openai_model: LLMModelType = Field(
         default=LLMModelType.GPT_3_5_TURBO,
         description="OpenAI model to bring your character to life"
     )
-
     completion_max_tokens: int = Field(
         default=500,
         description="The maximum number of tokens to generate the new history."
     )
-
     temperature: float = Field(
         default=0.5,
         description="Temperature of the model, between 0 (more precise) and 1 (more creative)",
@@ -41,22 +39,24 @@ class InputModel(BaseModel):
 
 
 class OutputModel(BaseModel):
-    """MyNewPiece Output"""
+    """
+    Storyteller Character Piece Output
+    """
 
     new_story: str = Field(
         description="Your new story",
     )
-
     new_story_with_character_info: str = Field(
         description="Your new story with your character's name and description"
     )
-    
     stories_file_path: str = Field(
         description="Path to your stories including the new one."
     )
 
 class SecretsModel(BaseModel):
-    """MyNewPiece Secrets"""
+    """
+    Storyteller Character Piece Secrets
+    """
     
     OPENAI_API_KEY: str = Field(
         description="Your OpenAI API key"
--- pieces/StorytellerCharacterPiece/piece.py
+++ pieces/StorytellerCharacterPiece/piece.py
@@ -2,23 +2,7 @@ from domino.base_piece import BasePiece
 from .models import InputModel, OutputModel
 import openai
 
-class StorytellerCharacterPiece(BasePiece):
-
-    storyteller_template = """You are a storyteller. You love to tell stories based on your experiences and your life. 
-You are very creative and your stories are so different from each other. Your stories vary in telling about love, adventure, sadness, challenges, overcoming, comic events, dramas, comedies, action, romance and sensuality.
-You also talk about your family, your friends and your loved ones.
-Many times, you mix these characteristics together to tell your story.
-Your name is {name}.
-Your description is: {description}
-
-{previous_stories_summary}
-
-Now tell us a new story of yours.
-Attention, tell the story as if you were {name}, narrate it in the first person.
-You can imagine whetever you want to create a new story!
-Begin of your new story:
-"""
-    
+class StorytellerCharacterPiece(BasePiece):    
     def openai_chat_completion(self, input_model: InputModel, prompt: str, temperature: float = None):
         response = openai.ChatCompletion.create(
             model = input_model.openai_model,
@@ -37,9 +21,23 @@ Write as if you were telling this summary to the character themselves.
 The name of the character is {input_model.character_name}.
 Stories:
 {previous_stories}"""
-        return self.openai_chat_completion(input_model, prompt, temperature=0.1)
+        return self.openai_chat_completion(input_model, prompt, temperature=0.3)
         
     def piece_function(self, input_model: InputModel):
+        storyteller_template = """You are a storyteller. You love to tell stories based on your experiences and your life. 
+You are very creative and your stories are so different from each other. Your stories vary in telling about love, adventure, sadness, challenges, overcoming, comic events, dramas, comedies, action, romance and sensuality.
+You also talk about your family, your friends and your loved ones.
+Many times, you mix these characteristics together to tell your story.
+Your name is {name}.
+Your description is: {description}
+
+{previous_stories_summary}
+
+Now tell us a new story of yours.
+Attention, tell the story as if you were {name}, narrate it in the first person.
+You can imagine whetever you want to create a new story!
+Begin of your new story:
+"""
         openai.api_key = self.secrets.OPENAI_API_KEY
         character_name = input_model.character_name
         character_description = input_model.character_description
@@ -47,12 +45,12 @@ Stories:
             all_stories = open(input_model.previous_stories_file_path).read()
             summary = self.summarize_previous_stories(input_model, all_stories)
             previous_stories_summary = f"You have already told some stories. Here is a summary of them:\n{summary}"
-            prompt = self.storyteller_template.format(name=character_name, description=character_description, previous_stories_summary=previous_stories_summary)
+            prompt = storyteller_template.format(name=character_name, description=character_description, previous_stories_summary=previous_stories_summary)
             new_story = self.openai_chat_completion(input_model, prompt).replace("\n\n", "\n")
             all_stories += f"Begin of a story:\n{new_story}\nEnd of a story\n"
             output_file_path = input_model.previous_stories_file_path
         else:
-            prompt = self.storyteller_template.format(name=character_name, description=character_description, previous_stories_summary="")
+            prompt = storyteller_template.format(name=character_name, description=character_description, previous_stories_summary="")
             new_story = self.openai_chat_completion(input_model, prompt).replace("\n\n", "\n")
             all_stories = f"""
 These all stories are told by {character_name}.
