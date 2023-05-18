d7eb86e038a889c539f663be30888c45f11fa8dc
Vinicius <vinicvaz95@gmail.com>
2023-05-18 14:27:52 -0300

    argtype

--- pieces/TextGeneratorPiece/models.py
+++ pieces/TextGeneratorPiece/models.py
@@ -1,14 +1,18 @@
 from pydantic import BaseModel, Field
 from enum import Enum
-from typing import List
+from typing import List, Union
+
 
 class LLMModelType(str, Enum):
     GPT_3_5_TURBO = "gpt-3.5-turbo"
     GPT_4 = "gpt-4"
 
+
 class InnerArgModel(BaseModel):
     arg_name: str
     arg_value: str
+    arg_type: Union[str, int, float, bool]
+
 
 class InputModel(BaseModel):
     """
@@ -22,7 +26,6 @@ class InputModel(BaseModel):
     prompt_args: List[InnerArgModel] = Field(
         default=None,
         description="List of arguments to insert into the prompt.",
-
     )
     # prompt_params: dict = Field(
     #     ...,
@@ -30,17 +33,16 @@ class InputModel(BaseModel):
     # )
     openai_model: LLMModelType = Field(
         default=LLMModelType.GPT_3_5_TURBO,
-        description="OpenAI model to bring your character to life"
+        description="OpenAI model to bring your character to life",
     )
     completion_max_tokens: int = Field(
-        default=500,
-        description="The maximum number of tokens to generate the text."
+        default=500, description="The maximum number of tokens to generate the text."
     )
     temperature: float = Field(
         default=0.3,
         description="Temperature of the model, between 0 (more precise) and 1 (more creative)",
         gt=0,
-        lt=1
+        lt=1,
     )
 
 
@@ -53,11 +55,10 @@ class OutputModel(BaseModel):
         description="The generated text",
     )
 
+
 class SecretsModel(BaseModel):
     """
     Text Generator Secrets
     """
-    
-    OPENAI_API_KEY: str = Field(
-        description="Your OpenAI API key"
-    )
+
+    OPENAI_API_KEY: str = Field(description="Your OpenAI API key")
